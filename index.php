222d8959e42cfb997922dc06b572809c8b8441e7
RodrigoRN1698 <56896622+RodrigoRN1698@users.noreply.github.com>
2019-10-22 17:40:55 -0500

    Se agrego el agregar un usuario

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
   $cual   = !empty($_POST['cual']) ? $_POST['cual'] : '';
   $select = HTML::selector($liga, 1, array('select'=>array('name'=>'cual', 'id'=>'algocual'),
 							 'option'=>array('value'=>'@[0]'),
-							 "option@si('$cual'=='@[0]')"=>array('selected'=>'selected')), array('depende'=>$liga)
+	HTML::forma( LIGA('usuarios'), 'Nuevo usuario');						 "option@si('$cual'=='@[0]')"=>array('selected'=>'selected')), array('depende'=>$liga)
 			   );
   $campos = array('cual'=>$select, '*', '-fecha');
   HTML::forma($liga, 'Modificar '.$tabla, $campos, $props, true);
